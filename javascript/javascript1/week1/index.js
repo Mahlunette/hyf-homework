dd812f2d25d8c3af910c4e37edb4d939155df611
mahlunette <mhtbmahdavifar@gmail.com>
2022-05-25 17:01:16 +0200

    add the dog age calculation

--- javascript/javascript1/week1/index.js
+++ javascript/javascript1/week1/index.js
@@ -1,7 +1,27 @@
 const yearOfBirth = 1985;
 const yearfuture = 2045;
 
-const age = yearfuture - yearOfBirth;
-console.log(age);
+const myAge = yearfuture - yearOfBirth;
+console.log(myAge);
+
+console.log("You will be " + myAge + " " + "years old in " + yearfuture + ".");
+
+
+const dogYearOfBirth = 2018; 
+const dogYearFuture = 2025;
+ const dogYear = dogYearFuture - dogYearOfBirth ;
+ const dogInHumanYear = dogYear * 7 ;
+
+let shouldShowResultInDogYears = true;
+if (shouldShowResultInDogYears == "true") {
+    console.log("my dog will be " + dogYear + "dog year old in " + " " + dogYearFuture + "." ) 
+    } else { 
+        console.log("my dog will be " + dogInHumanYear + " human year old in " + dogYearFuture + ".")
+
+    };
+ console.log(shouldShowResultInDogYears = false);
+ 
+
+
 
 
