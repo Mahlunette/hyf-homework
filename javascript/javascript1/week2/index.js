fd030910bb0b65a20f3b7f6907b436e0c99ff32b
mahlunette <mhtbmahdavifar@gmail.com>
2022-06-03 17:21:38 +0200

    add fullname function

---
+++ javascript/javascript1/week2/index.js
@@ -0,0 +1,13 @@
+// fullname function created 
+function getFullname(firstname,surname){
+    console.log(`Full name is ${firstname} ${surname}`);
+}
+
+getFullname("Mahtab", "Mahdavifar")
+
+// declaring 2 variables
+const fullname1 = "Mary Shelly"
+const fullname2 = "Johny Shelly"
+
+console.log(getFullname(fullname1,fullname2));
+
