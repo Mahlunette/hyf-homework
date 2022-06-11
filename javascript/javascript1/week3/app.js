b2af3fbd8f75e9f261a809dc2191a71dd6778d6f
mahlunette <mhtbmahdavifar@gmail.com>
2022-06-11 23:43:20 +0200

    estimate the arrival time

--- javascript/javascript1/week3/app.js
+++ javascript/javascript1/week3/app.js
@@ -10,6 +10,8 @@
     * - Author          : 
     * - Modification    : 
 **/
+
+// Part 1
 const names = [
     "Peter",
     "Ahmad",
@@ -19,13 +21,36 @@ const names = [
     "Samuel",
     "katrine",
     "Tala",
-  ];
-  const nameToRemove = "Ahmad";
-  
-  // Write some code here
-  console.log(names.indexOf(0))
-  names.splice(1,1)
-  
-  // Code done
-  
-  console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+];
+const nameToRemove = "Ahmad";
+
+// Write some code here
+console.log(names.indexOf(0))
+names.splice(1, 1)
+
+// Code done
+
+console.log(names); // ['Peter', 'Yana', 'kristina', 'Rasmus', 'Samuel', 'katrine', 'Tala']
+
+
+
+// Part 2
+//  When will we be there?
+const travelInformation = {
+    speed: 50,
+    destinationDistance: 432,
+};
+
+function estimateArrivalTime(speed, destinationDistance) {
+    // to calculate the hours
+    var decimalTime = (destinationDistance / speed);
+    //   using floor and ceil to round numbers 
+    var hours = Math.floor(decimalTime)
+    var minutesInDecimal = (decimalTime - hours) * 60
+    var minutes = Math.floor(minutesInDecimal)
+
+    return `${hours} hour and ${minutes} minutes`
+    // This should give us 8 hours and 38 minutes 
+}
+
+console.log(estimateArrivalTime(50, 432))
