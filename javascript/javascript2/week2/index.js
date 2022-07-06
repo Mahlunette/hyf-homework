564a6b7d012cd47990e583936e1a6a47e3a3014c
Mahtab Mahdavifar <mhtbmahdavifar@gmail.com>
2022-07-06 19:26:38 +0200

    add the necessary files

--- javascript/javascript2/week2/index.js
+++ javascript/javascript2/week2/index.js
@@ -1,8 +1,19 @@
-let numbers = [1, 2, 3, 4];
-let newNumbers = [];
+// let numbers = [1, 2, 3, 4];
+// let newNumbers = [];
 
-const oddNumber = numbers.map(function(number){
-return newNumbers.push(number % 2 === 0)
-});
+// const oddNumber = numbers.map(function(number){
+// return newNumbers.push(number % 2 === 0)
+// });
 
-console.log("The doubled numbers are", newNumbers);
+// console.log("The doubled numbers are", newNumbers);
+
+
+let numbers = [1, 2, 3, 4]
+let newNumbers =[]
+
+numbers.map(function(number){
+    if (numbers % 2 != 0) {
+      newNumbers = numbers * 2;
+    }
+  })
+  console.log(newNumbers);
