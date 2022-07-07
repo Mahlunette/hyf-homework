69537dd8f767479ccc47bf54ec6b2ed3960c3741
Mahtab Mahdavifar <mhtbmahdavifar@gmail.com>
2022-07-07 16:22:11 +0200

    add the changes

--- javascript/javascript2/week2/index.js
+++ javascript/javascript2/week2/index.js
@@ -1,19 +1,25 @@
-// let numbers = [1, 2, 3, 4];
-// let newNumbers = [];
+// Exercide 1:double the odd numbers/ throw the even numbers
+let numbers = [1, 2, 3, 4];
+let newNumbers = []
 
-// const oddNumber = numbers.map(function(number){
-// return newNumbers.push(number % 2 === 0)
-// });
+const oddNumber = numbers.filter(function(number){
+if (number % 2 === 0){
+  newNumbers.push(number * 2)
+}
+});
 
-// console.log("The doubled numbers are", newNumbers);
+console.log(oddNumber);
 
 
-let numbers = [1, 2, 3, 4]
-let newNumbers =[]
+// let numbers = [1, 2, 3, 4]
+// let newNumbers =[]
 
-numbers.map(function(number){
-    if (numbers % 2 != 0) {
-      newNumbers = numbers * 2;
-    }
-  })
-  console.log(newNumbers);
+// numbers.map(function(i){
+//     if (numbers[i] % 2 != 0) {
+//       newNumbers[i] = numbers[i] * 2;
+//     }else{
+//       newNumbers.delete
+//     }
+//   })
+//   console.log(newNumbers);
+
