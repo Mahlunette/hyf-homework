0962bbf7cbff3238dcb4b624d8c28e3064c74063
Mahtab Mahdavifar <mhtbmahdavifar@gmail.com>
2022-07-16 17:31:03 +0200

    add ex2

---
+++ javascript/javascript2/week3/ExerciseTwo/EX2.js
@@ -0,0 +1,45 @@
+// Create an array with 3 items. All items should be functions.
+// Iterate through the array and call all the functions.
+
+const function1 = function(){
+  console.log("FunctionOne");
+}
+
+const function2 = function(){
+  console.log("FunctionTwo");
+}
+
+const function3 = function(){
+  console.log("FunctionThree");
+}
+
+const functionArray = [function1, function2, function3]
+
+functionArray.forEach((item, index) =>{
+console.log(item,index)
+})
+
+
+// Create a function as a const and
+// try creating a function normally.
+// Call both functions. Read up on this if you are interested:
+
+const multiply = function(num1, num2) {
+  return num1 * num2
+}
+
+function division(num1, num2){
+   return num1 / num2
+}
+
+console.log(multiply(4,8)); //output = 32
+console.log(division(8,4)); //output = 2
+
+
+// Create an object that has a key whose value is a
+// function. Try calling this function.
+
+const sum = (a,b)=> {a+b}
+const subjects = {maths: sum}
+
+console.log(subjects.maths());
